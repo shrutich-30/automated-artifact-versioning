d082aa0591ff008696b6d5cb9d0679d23bed23fd
shrutich-30 <shrutichaurasiya201@gmail.com>
2026-06-12 15:44:57 +0530

    update flask response

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Artifact Versioning Pipeline Running 🚀"
+    return "New Artifact Version Pipeline 🚀"
 
 
 if __name__ == "__main__":
